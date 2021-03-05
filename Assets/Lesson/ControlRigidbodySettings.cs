696ee48a16d934da09488f679e5e250d8b727bf8
Mert CİLLİ <mertcilli@outlook.com.tr>
2021-03-05 07:26:58 +0300

    +created: rigidbody operations.

---
+++ Assets/Lesson/ControlRigidbodySettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    [CreateAssetMenu(menuName = "Lessons/Lessons1/Control Rigidboty Settings")]
+    public class ControlRigidbodySettings : ScriptableObject
+    {
+
+        [SerializeField] private Vector3 _jumpForce;
+
+        public Vector3 JumpForce
+        {
+            get { return _jumpForce; }
+        }
+    }
+}
