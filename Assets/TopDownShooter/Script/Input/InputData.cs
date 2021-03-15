47743eb90dfe5a08df5433a50253419a25662439
Mert CİLLİ <mertcilli@outlook.com.tr>
2021-03-15 07:40:33 +0300

    +created : basic input system

---
+++ Assets/TopDownShooter/Script/Input/InputData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace topDownShooter.PlayerInput
+{
+    [CreateAssetMenu(menuName = "topDown Shooter/Input/Input Data")]
+public class InputData : ScriptableObject
+{
+    public float Horizontal;
+    public float Vertical;
+}
+}
