c144fe2605ab785d6e894052e27e1d0e29686dab
Mert CİLLİ <mertcilli@outlook.com.tr>
2021-04-05 05:21:16 +0300

    -refactor input system

--- Assets/TopDownShooter/Script/Input/InputData.cs
+++ Assets/TopDownShooter/Script/Input/InputData.cs
@@ -9,5 +9,68 @@ public class InputData : ScriptableObject
 {
     public float Horizontal;
     public float Vertical;
+
+    [Header("Axis Base Control")] 
+    [SerializeField] private bool _axisActive;
+    [SerializeField] private string AxisNameHorizontal;
+    [SerializeField] private string AxisNameVeritcal;
+
+    [Header("Key Base Control")] 
+    [SerializeField] private bool _keyBaseHorizontalActive;
+    [SerializeField] private KeyCode positiveHorizontalKeyCode;
+    [SerializeField] private KeyCode negativeHorizontalKeyCode;
+    
+    [SerializeField] private bool _keyBaseVerticalActive;
+    [SerializeField] private KeyCode positiveVerticalKeyCode;
+    [SerializeField] private KeyCode negativeVerticalKeyCode;
+
+    [SerializeField] private float _increaseAmount = 0.015f;
+
+    public void ProcessInput()
+    {
+        if (_axisActive)
+        {
+            Horizontal = Input.GetAxis(AxisNameHorizontal);
+            Vertical = Input.GetAxis(AxisNameVeritcal);
+        }
+        else
+        {
+            if (_keyBaseHorizontalActive)
+            {
+                KeyBaseAxisControl(ref Horizontal, positiveHorizontalKeyCode, negativeHorizontalKeyCode);
+            }
+
+            if (_keyBaseVerticalActive)
+            {
+                KeyBaseAxisControl(ref Vertical, positiveVerticalKeyCode, negativeVerticalKeyCode);
+            }
+        }
+    }
+
+    private void KeyBaseAxisControl(ref float value, KeyCode positive, KeyCode negative)
+    {
+        bool positiveActive = Input.GetKey(positive);
+        bool negativeActive = Input.GetKey(negative);
+
+        if (positiveActive)
+        {
+            value += _increaseAmount;
+            //value = Mathf.Lerp(value, 1, Time.time * _increaseAmount);
+        }
+        else if (negativeActive)
+        {
+            value -= _increaseAmount;
+            //value = Mathf.Lerp(value, -1, Time.time * _increaseAmount);
+        }
+        else
+        {
+            value = 0;
+            //value = Mathf.Lerp(value, 0, Time.time * _increaseAmount);
+        }
+    }
+    
+
+
+
 }
 }
--- Assets/TopDownShooter/Script/Input/InputManager.cs
+++ Assets/TopDownShooter/Script/Input/InputManager.cs
@@ -8,16 +8,23 @@ namespace topDownShooter.PlayerInput
 
 public class InputManager : MonoBehaviour
 {
-    [SerializeField] private InputData _inputData;
-    [SerializeField] private InputData _rotationInputData;
-    private Vector3 _lastMouseInput;
+    //[SerializeField] private InputData _rotationInputData;
+    //private Vector3 _lastMouseInput;
+    [SerializeField] private InputData[] _inputDataArray;
     private void Update()
     {
-        _inputData.Horizontal = Input.GetAxis("Horizontal");
+
+        for (int i = 0; i < _inputDataArray.Length; i++)
+        {
+            _inputDataArray[i].ProcessInput();
+        }
+        
+        /*_inputData.Horizontal = Input.GetAxis("Horizontal");
         _inputData.Vertical = Input.GetAxis("Vertical");
         
         _rotationInputData.Horizontal = (Input.mousePosition.x - _lastMouseInput.x);
         _lastMouseInput = Input.mousePosition;
+        */
     }
 }
 }
