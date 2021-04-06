151df5d1e72878beba815d881e29a12dffa149b3
Mert CİLLİ <mertcilli@outlook.com.tr>
2021-04-06 07:38:33 +0300

    +fix : rotation controller for tur

--- Assets/TopDownShooter/Script/Camera/CameraController.cs
+++ Assets/TopDownShooter/Script/Camera/CameraController.cs
@@ -11,31 +11,32 @@ namespace topDownShooter.Camera
        [SerializeField] private CameraSettings _cameraSettings;
        [SerializeField] private Transform _targetTransform;
        [SerializeField] private Transform _cameraTransform;
-       public ShootingManager _ShootingManager;
+       //public ShootingManager _ShootingManager;
 
        private void Update()
        {
           CameraRotationFollow();
-          //CameraMovementFollow();
-          if (Input.GetKeyDown(KeyCode.Space))
-          {
-              Debug.Log("try shoot");
-//              _ShootingManager.Shoot(_cameraTransform.position, _cameraTransform.forward);
-          }
+          CameraMovementFollow();
+          //if (Input.GetKeyDown(KeyCode.Space)) { Debug.Log("try shoot"); _ShootingManager.Shoot(_cameraTransform.position, _cameraTransform.forward); }
        }
 
        private void CameraRotationFollow()
        {
            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, 
-               Quaternion.LookRotation(_targetTransform.position- _cameraTransform.position), 
+               Quaternion.LookRotation(_targetTransform.forward), 
                Time.deltaTime * _cameraSettings.RotationLerpSpeed);
        }
 
        private void CameraMovementFollow()
        {
+           Vector3 offset = (_cameraTransform.right * _cameraSettings.PositionOffSet.x)
+                            + (_cameraTransform.up * _cameraSettings.PositionOffSet.y)
+                            + (_cameraTransform.forward * _cameraSettings.PositionOffSet.z);
+
            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, 
-               _targetTransform.position + _cameraSettings.PositionOffSet, 
-               Time.deltaTime * _cameraSettings.PositionLerp );
+               _targetTransform.position + offset, 
+               Time.deltaTime * _cameraSettings.PositionLerp);   
+           
        }
     }    
 }
--- Assets/TopDownShooter/Script/PlayerControl/Movement/PlayerMovementController.cs
+++ Assets/TopDownShooter/Script/PlayerControl/Movement/PlayerMovementController.cs
@@ -12,18 +12,22 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private InputData _inputData;
     [SerializeField] private PlayerMovementSettings _playerMovementSettings;
-    [SerializeField] private ControlRigidbodySettings _settings;
+    [SerializeField] private Transform _targetTransform;
+    //[SerializeField] private ControlRigidbodySettings _settings;
 
     private void Update()
     {
         _rigidbody.MovePosition(_rigidbody.position + (_rigidbody.transform.forward * _inputData.Vertical * _playerMovementSettings.VerticalSpeed));
-        _rigidbody.MovePosition(_rigidbody.position + (_rigidbody.transform.right * _inputData.Horizontal * _playerMovementSettings.HorizontalSpeed));
+        _targetTransform.Rotate(0,_inputData.Horizontal * _playerMovementSettings.HorizontalSpeed,0, Space.Self);
+        //_rigidbody.MovePosition(_rigidbody.position + (_rigidbody.transform.right * _inputData.Horizontal * _playerMovementSettings.HorizontalSpeed));
         
-        bool spaceGetDown = Input.GetKeyDown(KeyCode.Space);
+        /*
+         bool spaceGetDown = Input.GetKeyDown(KeyCode.Space);
         if (spaceGetDown)
         {
             _rigidbody.AddForce(_settings.JumpForce, ForceMode.Impulse);
         }
+        */
     }
 }    
 } 
